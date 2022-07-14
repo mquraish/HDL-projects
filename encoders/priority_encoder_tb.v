8a5847638a4755828e7bdadae09341b0ed4e1f76
mquraish <mquraish@asu.edu>
2022-07-14 11:23:08 -0400

    priority encoder testbench updated

--- encoders/priority_encoder_tb.v
+++ encoders/priority_encoder_tb.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 module priority_encoder_tb;
 
 reg [7:0] in;
@@ -9,23 +10,21 @@ priority_encoder DUT(
     .out(out)
 );
 
+integer i;
+
 initial begin
 #100
-in = 8'b0000_0000;
+in = 8'b00000000;
 #1 $display("Input: %b   Output: %b",in,out);
-#10
-in = 8'b0000_0001;
+for(i=1; i<130; i=i<<1) begin
+#10 in = i;
 #1 $display("Input: %b   Output: %b",in,out);
-#10
-in = 8'b0000_1000;
+#10 in = i+1;
 #1 $display("Input: %b   Output: %b",in,out);
+end
 #10
-in = 8'b0000_0000;
+in = 8'b11111111;
 #1 $display("Input: %b   Output: %b",in,out);
-#10
-in = 8'b1000_0000;
-#1 $display("Input: %b   Output: %b",in,out);
-#10
 $finish;
 end
 endmodule
